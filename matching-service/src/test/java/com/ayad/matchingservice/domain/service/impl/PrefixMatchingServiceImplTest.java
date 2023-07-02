ec3bad3cfa5c2e10dcf98371b4c22233aa0a29de
mohammedayad <mohammedayad611@gmail.com>
2023-07-02 23:02:55 +0200

    Handle unit-test and integration-test

--- matching-service/src/test/java/com/ayad/matchingservice/domain/service/impl/PrefixMatchingServiceImplTest.java
+++ matching-service/src/test/java/com/ayad/matchingservice/domain/service/impl/PrefixMatchingServiceImplTest.java
@@ -1,21 +1,24 @@
 package com.ayad.matchingservice.domain.service.impl;
 
+import com.ayad.matchingservice.common.exception.MatchingException;
+import com.ayad.matchingservice.common.utils.MatchingUtility;
 import com.ayad.matchingservice.domain.model.PrefixEntity;
 import com.ayad.matchingservice.domain.repository.ifc.PrefixRepository;
 import com.ayad.matchingservice.domain.service.ifc.PrefixMatchingService;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
+import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,5 +53,51 @@ public class PrefixMatchingServiceImplTest {
 
         assertThat(prefixes.size(), is(2));
         assertThat(prefixes, containsInAnyOrder("abc", "def"));
+    }
+
+    /**
+     * Tests the {@link PrefixMatchingService#buildTrie(List)} method to ensure that it correctly builds a prefix trie.
+     */
+    @Test
+    public void testBuildTrie() {
+        // Set up the test data
+        List<String> prefixes = Arrays.asList("abc", "def", "ghij");
+
+        // Call the method being tested
+        prefixMatchingService.buildTrie(prefixes);
+
+        // Verify the results
+        assertThat(prefixMatchingService.findLongestPrefix("abcdef"), is("abc"));
+        assertThat(prefixMatchingService.findLongestPrefix("defhi"), is("def"));
+        assertThat(prefixMatchingService.findLongestPrefix("abc"), is("abc"));
+        assertThat(prefixMatchingService.findLongestPrefix("def"), is("def"));
+        assertThat(prefixMatchingService.findLongestPrefix("ghij"), is("ghij"));
+    }
+
+    /**
+     * Tests the {@link PrefixMatchingService#findLongestPrefix(String)} method to ensure that it correctly finds the longest prefix that matches a given input string.
+     *
+     * @throws MatchingException if no prefix matching the input string is found
+     */
+    @Test
+    public void testFindLongestPrefix() throws MatchingException {
+        // Set up the test data
+        List<String> prefixes = Arrays.asList("abc", "def", "ghij");
+        prefixMatchingService.buildTrie(prefixes);
+
+        // Call the method being tested
+        String longestPrefix = prefixMatchingService.findLongestPrefix("abctest");
+        // Verify the results
+        assertThat(longestPrefix, is("abc"));
+        // Test for an input string with no matching prefix
+        try {
+            prefixMatchingService.findLongestPrefix("EEXCabcffffdef");
+            fail("Expected MatchingException to be thrown");
+        } catch (MatchingException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getErrorCode(), is(MatchingUtility.CONSTRAINT_VIOLATIONS));
+            assertThat(e.getErrorMessage(), is(MatchingUtility.NO_PREFIX_MATCHING_FOUND));
+        }
+
     }
 }
---
+++ matching-service/src/test/java/com/ayad/matchingservice/resources/rest/controller/PrefixMatchingControllerIntegrationTest.java
@@ -0,0 +1,73 @@
+package com.ayad.matchingservice.resources.rest.controller;
+
+/**
+ * Integration tests for the {@link PrefixMatchingController} class.
+ */
+
+import com.ayad.matchingservice.common.exception.MatchingException;
+import com.ayad.matchingservice.domain.service.ifc.PrefixMatchingService;
+import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+
+
+/**
+ * Integration tests for the {@link PrefixMatchingController} class.
+ * H2 in-memory Database for storing the prefixes.
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PrefixMatchingControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private static PrefixMatchingService prefixMatchingService;
+
+
+    /**
+     * Tests the {@link PrefixMatchingController#getAllPrefixes()} method by calling the REST API endpoint and verifying the response.
+     * the prefixes are retrieved from H2 in-memory database
+     */
+    @Test
+    public void testGetAllPrefixes() {
+        ResponseEntity<List> responseEntity = restTemplate.getForEntity("/api/prefix-matching/v1/prefixes", List.class);
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+        assertThat((List<String>)responseEntity.getBody(), hasItems("2y3fKTS", "4VdwEEXC8", "AWMa4vvOf"));
+
+    }
+
+    /**
+     * Tests the {@link PrefixMatchingController#findLongestPrefix(String)} method by calling the REST API endpoint and verifying the response.
+     */
+    @Test
+    public void testFindLongestPrefix() throws MatchingException {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/api/prefix-matching/v1/longestPrefix/2y3fKTSfter", String.class);
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+        assertThat(responseEntity.getBody(), is("2y3fKTS"));
+    }
+
+
+    /**
+     * Tests the case where the {@link PrefixMatchingController#findLongestPrefix(String)} method throws a {@link MatchingException} by calling the REST API endpoint and verifying the response.
+     */
+    @Test
+    public void testFindLongestPrefixThrowsMatchingException() throws MatchingException {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/api/prefix-matching/v1/longestPrefix/EEXC8ffffff", String.class);
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+        assertThat(responseEntity.getBody(), is("No Prefix Matching has been found"));
+    }
+}
