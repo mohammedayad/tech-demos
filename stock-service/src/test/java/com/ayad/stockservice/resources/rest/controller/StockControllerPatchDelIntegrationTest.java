d51db801dc12ddc126d554fe6374137208c011c9
mohammedayad <mohammedayad611@gmail.com>
2023-08-01 01:48:08 +0200

    add missed test case

--- stock-service/src/test/java/com/ayad/stockservice/resources/rest/controller/StockControllerPatchDelIntegrationTest.java
+++ stock-service/src/test/java/com/ayad/stockservice/resources/rest/controller/StockControllerPatchDelIntegrationTest.java
@@ -3,6 +3,14 @@ package com.ayad.stockservice.resources.rest.controller;
 import com.ayad.stockservice.domain.model.dtos.ProblemDto;
 import com.ayad.stockservice.domain.model.dtos.StockDto;
 import com.ayad.stockservice.domain.model.dtos.StockPriceDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +30,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class StockControllerPatchIntegrationTest {
+public class StockControllerPatchDelIntegrationTest {
 
     private RestTemplate restTemplate;
     @LocalServerPort
@@ -45,7 +53,10 @@ public class StockControllerPatchIntegrationTest {
         StockPriceDto newPrice = new StockPriceDto();
         BigDecimal price = BigDecimal.valueOf(150.00);
         newPrice.setCurrentPrice(price);
-        String url = "http://localhost:" + port + "/stock-service/v1/api/stocks/{id}";
+       // String url = "http://localhost:" + port + "/stock-service/v1/api/stocks/{id}";
+        StringBuilder url = new StringBuilder("http://localhost:");
+        url.append(port);
+        url.append("/stock-service/v1/api/stocks/{id}");
         // Test success case
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -53,7 +64,7 @@ public class StockControllerPatchIntegrationTest {
         HttpEntity<StockPriceDto> requestEntity = new HttpEntity<>(newPrice, headers);
 
         ResponseEntity<StockDto> responseEntity = restTemplate.exchange(
-                url,
+                url.toString(),
                 HttpMethod.PATCH,
                 requestEntity,
                 StockDto.class,
@@ -73,12 +84,15 @@ public class StockControllerPatchIntegrationTest {
             StockPriceDto newPrice = new StockPriceDto();
             BigDecimal price = BigDecimal.valueOf(150.00);
             newPrice.setCurrentPrice(price);
-            String url = "http://localhost:" + port + "/stock-service/v1/api/stocks/{id}";
+           // String url = "http://localhost:" + port + "/stock-service/v1/api/stocks/{id}";
+            StringBuilder url = new StringBuilder("http://localhost:");
+            url.append(port);
+            url.append("/stock-service/v1/api/stocks/{id}");
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity<StockPriceDto> requestEntity = new HttpEntity<>(newPrice, headers);
             restTemplate.exchange(
-                    url,
+                    url.toString(),
                     HttpMethod.PATCH,
                     requestEntity,
                     ProblemDto.class,
@@ -90,4 +104,36 @@ public class StockControllerPatchIntegrationTest {
 
 
     }
+
+
+
+    @Test
+    public void testDeleteStock_Success() throws Exception {
+        // Create a new stock to use in the test
+        StockDto stock = new StockDto();
+        stock.setName("Test Stock");
+        stock.setCurrentPrice(BigDecimal.valueOf(10.99));
+        //String url = "http://localhost:" + port + "/stock-service/v1/api/stocks/";
+        StringBuilder url = new StringBuilder("http://localhost:");
+        url.append(port);
+        url.append("/stock-service/v1/api/stocks/");
+
+        String stockJson = new ObjectMapper().writeValueAsString(stock);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url.toString());
+        httpPost.setEntity(new StringEntity(stockJson));
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+        String responseJson = EntityUtils.toString(httpResponse.getEntity());
+        long createdStockId = new ObjectMapper().readTree(responseJson).get("id").asLong();
+        // Perform DELETE request using the ID of the created stock
+        HttpDelete httpDelete = new HttpDelete(url.append(createdStockId).toString());
+        CloseableHttpResponse response = httpClient.execute(httpDelete);
+        // Verify response status code
+        assertThat(response.getStatusLine().getStatusCode(), is(HttpStatus.NO_CONTENT.value()));
+        // Close resources
+        response.close();
+        httpClient.close();
+    }
 }
+
